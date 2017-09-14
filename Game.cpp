45cf1daf7fdd483c321a2178f4a85af93e3209c2
anthony.w.chuah@gmail.com <anthony.w.chuah@gmail.com>
2017-09-14 23:04:53 +0100

    Wrote up SDL event handler to take user input

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,75 @@
 #include "Game.h"
 
-Game::Game() {
+#include <thread>
+#include <chrono>
+
+Game::Game() : board_(Game::TIMESTEPS_PER_TICK), view_(&board_) {}
+
+void Game::launchAllThreads() {
+  this->isRunning = true;
+  std::thread controlThread(this->launchControlThread);
+  std::thread gameThread(this->launchGameThread);
+  std::thread clockThread(this->launchClockThread);
+  controlThread.join();
+  gameThread.join();
+  clockThread.join();
+}
+
+void Game::launchControlThread() {
+  SDL_Event e;
+  while (this->isRunning) {
+    while (SDL_PollEvent(&e) != 0) {
+      if (e.type == SDL_QUIT) {
+	isRunning = false;
+      } else if (e.type == SDL_KEYDOWN) {
+	switch (e.key.keysym.sym) {
+	case SDLK_LEFT:
+	  cmdQueue_.push(CMD_LEFT);
+	  break;
+	case SDLK_RIGHT:
+	  cmdQueue_.push(CMD_RIGHT);
+	  break;
+	case SDLK_q:
+	  cmdQueue_.push(CMD_ANTI);
+	  break;
+	case SDLK_r:
+	  cmdQueue_.push(CMD_CLOCK);
+	  break;
+	case SDLK_DOWN:
+	  cmdQueue_.push(CMD_DOWN);
+	  break;
+	case SDLK_SPACE:
+	  cmdQueue_.push(CMD_DROP);
+	  break;
+	default:
+	  break;
+	}
+      }
+    }
+  }
+}
+
+void Game::launchGameThread() {
+  while (this->isRunning) {
+    std::unique_lock<std::mutex> locker(mutexClock);
+    condvarClock.wait(locker);
+    this->updateModelForThisFrame();
+    this->renderView();
+  }
+}
+
+void Game::launchClockThread() {
+  while (this->isRunning) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(Game::MILLISECS_PER_TIMESTEP));
+    condvarClock.notify_all();
+  }
+}
+
+// Private member functions
+
+void Game::updateModelForThisFrame() {
+  // If cmdQueue is not empty, pop one command off and do board's timestep once
+}
+
+void Game::renderView() {
 }
--- Game.h
+++ Game.h
@@ -1,5 +1,8 @@
 #ifndef GAME_H
-#define GAME_h
+#define GAME_H
+
+#include <mutex>
+#include <condition_variable>
 
 #include "CircularBuffer.h"
 
@@ -19,32 +22,46 @@ Commands:
 */
 
 class Game {
-  
+
+  static constexpr int TIMESTEPS_PER_TICK = 8;
   static constexpr int MILLISECS_PER_TIMESTEP = 50; // 20 frames per second
   static constexpr int COMMAND_BUFFER = 64;
-  View view_;
+  static constexpr int CMD_LEFT = 1;
+  static constexpr int CMD_RIGHT = 2;
+  static constexpr int CMD_ANTI = 3;
+  static constexpr int CMD_CLOCK = 4;
+  static constexpr int CMD_DOWN = 5;
+  static constexpr int CMD_DROP = 6;
   Board board_;
+  View view_;
   int nextCommand_ = 0;
+  bool isRunning = false;
+  std::mutex mutexClock;
+  std::condition_variable condvarClock;
   CircularBuffer<int, COMMAND_BUFFER> cmdQueue_;
+
+  void updateModelForThisFrame();
+  void renderView();
   
  public:
   
   Game(); // default constructor override
   void launchAllThreads(); // calls all 3 launch functions below
-  void launchControllerThread(); // polls indefinitely to accept user input
+  // void launchControllerThread(); // polls indefinitely to accept user input
   // whenever user makes an input, this is buffered for the next timestep, and all other
   // inputs are ignored until that timestep is processed
-  void launchGameAndViewThread(); // unlike the Controller thread, does not poll
+  // void launchGameAndViewThread(); // unlike the Controller thread, does not poll
   // instead, it blocks waiting until a timestep happens (Clock thread notifies)
   // when unblocked, it figures out what the user's command since the last timestep was
   // with that command, it executes the change to the game
   // then once the game is changed, it updates the View (graphics)
   // NOTE: could be worth separating Game and View threads if in danger of lagging
   // graphics may lag but the game logic must not
+  void launchControlThread();
+  void launchGameThread(); // does Model/View/Controller all together
   void launchClockThread(); // the clock gives the illusion of smooth real-time playing
   // WARNING: if processing takes longer than the timestep (50 ms) you could skip a timestep
   // this should never happen on a normal computer however
-  void launchGameThread(); // does Model/View/Controller all together
   
 };
 
